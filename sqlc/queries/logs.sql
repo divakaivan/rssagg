5320bfcac15f6e29361b5ae3a4c8cab67e451715
divakaivan <timee98@outlook.com>
2025-03-12 16:50:55 +0900

    add remote_addr tracking to each api call

--- sqlc/queries/logs.sql
+++ sqlc/queries/logs.sql
@@ -2,8 +2,9 @@
 insert into logs (
     timestamp,
     caller_user_id,
+    remote_addr,
     method,
     url,
     status,
     duration_ms
-) values ($1, $2, $3, $4, $5, $6);
+) values ($1, $2, $3, $4, $5, $6, $7);
--- sqlc/schema/007_logging.sql
+++ sqlc/schema/007_logging.sql
@@ -4,6 +4,7 @@ create table logs (
     id serial primary key,
     timestamp timestamp not null,
     caller_user_id uuid not null references users(id),
+    remote_addr text not null,
     method text not null,
     url text not null,
     status text not null,
